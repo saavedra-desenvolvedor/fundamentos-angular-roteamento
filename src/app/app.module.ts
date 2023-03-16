6e58bb0bebd33d1f8ac5bff4915dec8240c0d8fd
saavedra-desenvolvedor <66433098+saavedra-desenvolvedor@users.noreply.github.com>
2023-03-16 10:38:14 -0300

    primeiro-roteamento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
+import 'zone.js';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
+import { BrowserModule, platformBrowser } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
 
 @NgModule({
-  imports: [CommonModule, AppRoutingModule, PrimeiraPaginaComponent],
-  declarations: [],
+  imports: [CommonModule, AppRoutingModule, BrowserModule],
+  providers: [],
+  declarations: [PrimeiraPaginaComponent, AppComponent],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
