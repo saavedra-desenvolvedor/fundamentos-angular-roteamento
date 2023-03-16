3387bee746306d0ed1d062f48349cb3e94a1c795
saavedra-desenvolvedor <66433098+saavedra-desenvolvedor@users.noreply.github.com>
2023-03-16 11:29:41 -0300

    roteamento entre paginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,13 @@
 import 'zone.js';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
-import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
-import { BrowserModule, platformBrowser } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  imports: [CommonModule, AppRoutingModule, BrowserModule],
+  declarations: [AppComponent],
+  imports: [AppRoutingModule, BrowserModule],
   providers: [],
-  declarations: [PrimeiraPaginaComponent, AppComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
