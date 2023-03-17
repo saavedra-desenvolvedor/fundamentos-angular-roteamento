8df0b380471d36fac63b9bd50c499550d6439323
saavedra-desenvolvedor <66433098+saavedra-desenvolvedor@users.noreply.github.com>
2023-03-17 13:24:52 -0300

    pagina protegida

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,33 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { PaginaComParametroComponent } from './pagina-com-parametro/pagina-com-parametro.component';
-import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
-import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
-import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth.guard';
+import { LoginComponent } from './login/login.component';
+import { PaginaComParametroComponent } from './pagina-com-parametro/pagina-com-parametro.component';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
+import { PaginaProtegidaComponent } from './pagina-protegida/pagina-protegida.component';
+import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
+import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
 
-const routes: Routes = [
-  { path: 'primeira-pagina', component: PrimeiraPaginaComponent },
-  { path: 'segunda-pagina', component: SegundaPaginaComponent },
-  { path: '', redirectTo: 'primeira-pagina', pathMatch: 'full' },
-  { path: "pagina-com-parametro/:id", component: PaginaComParametroComponent},
-  { path: 'lazy-loading', loadChildren: () => import("./lazy-loading/lazy-loading.module").then(m => m.LazyLoadingModule)},
-  { path: "**", component: PaginaNaoEncontradaComponent}
+const routes: Routes = [
+  { path: 'primeira-pagina', component: PrimeiraPaginaComponent },
+  { path: 'segunda-pagina', component: SegundaPaginaComponent },
+  { path: '', redirectTo: 'primeira-pagina', pathMatch: 'full' },
+  { path: 'pagina-com-parametro/:id', component: PaginaComParametroComponent },
+  {
+    path: 'lazy-loading',
+    loadChildren: () =>
+      import('./lazy-loading/lazy-loading.module').then(
+        (m) => m.LazyLoadingModule
+      ),
+  },
+  { path: 'pagina-protegida', component: PaginaProtegidaComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent},
+  { path: '**', component: PaginaNaoEncontradaComponent },
 ];
 
 @NgModule({
-  declarations: [],
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
+import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
+import { PaginaComParametroComponent } from './pagina-com-parametro/pagina-com-parametro.component';
+import { PaginaProtegidaComponent } from './pagina-protegida/pagina-protegida.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [AppRoutingModule, BrowserModule],
+  declarations: [
+    AppComponent,
+    PrimeiraPaginaComponent,
+    SegundaPaginaComponent,
+    PaginaNaoEncontradaComponent,
+    PaginaComParametroComponent,
+    PaginaProtegidaComponent,
+    LoginComponent,
+  ],
+  imports: [AppRoutingModule, BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
