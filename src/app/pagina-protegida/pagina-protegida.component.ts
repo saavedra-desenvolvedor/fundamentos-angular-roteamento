7eb5ce1c66618a28db5b0450a0f1689fd162ce08
saavedra-desenvolvedor <66433098+saavedra-desenvolvedor@users.noreply.github.com>
2023-03-17 15:53:29 -0300

    auth guard (login e logout)

--- src/app/pagina-protegida/pagina-protegida.component.ts
+++ src/app/pagina-protegida/pagina-protegida.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-pagina-protegida',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaginaProtegidaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth : AuthService, private router : Router) { }
 
   ngOnInit() {
+  }
+  logout() {
+    this.auth.logout();
+    this.router.navigate([""]);
   }
 
 }
